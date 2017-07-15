a1c7efbc66b983c7ce794b8a4d4f7117564db355
Eric Jackson <ejackson@ashevillenc.gov>
2017-07-15 14:38:54 -0400

    Using a more up-to-date starting point

--- schema.js
+++ schema.js
@@ -1,11 +1,77 @@
 const baseSchema = `
   # the schema allows the following query:
+  type Employee {
+    id: Int!
+    active: Boolean!
+    name: String!
+    email: String!
+    position: String
+    department: String
+    division: String
+    current_review: Int
+    last_reviewed: String
+    review_by: String
+    supervisor_id: Int!
+    supervisor_name: String
+    supervisor_email: String
+    employees: [Employee]
+    reviews: [Review]
+  }
+
+  type Review {
+    id: Int!
+    status: String!
+    status_date: String
+    supervisor_id: Int!
+    employee_id: Int!
+    position: String
+    periodStart: String
+    periodEnd: String
+    reviewer_name: String
+    employee_name: String
+    questions: [Question]
+    responses: [Response]
+  }
+
+  input ReviewInput {
+    periodStart: String!
+    periodEnd: String!
+    questions: [QuestionInput]!
+    responses: [ResponseInput]!
+  }
+
+  type Question {
+    id: Int!
+    type: String!
+    question: String!
+    answer: String
+    required: Boolean
+  }
+
+  input QuestionInput {
+    id: Int!
+    answer: String
+  }
+
+  type Response {
+    question_id: Int
+    review_id: Int!
+    Response: String
+  }
+
+  input ResponseInput {
+    question_id: Int
+    Response: String
+  }
+
   type Query {
-    version: String
+    employee ( id: Int ): Employee
+    review ( id: Int, employee_id: Int ): Review
+  }
+
+  type Mutation {
+    updateReview (id: Int, review: ReviewInput!): Review
   }
 `;
 
-module.exports = baseSchema.concat(
-  require('./data/simplicity').schema,
-  require('./data/mda').schema
-);
+module.exports = baseSchema;
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 require('dotenv').config();
 
+
 console.log('Build the schema');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
@@ -13,39 +14,38 @@ const executableSchema = makeExecutableSchema({
   resolvers,
 });
 console.log('Done');
-for (const thing in executableSchema) {
-  if (executableSchema.hasOwnProperty(thing)) {
-    console.log('Name is ' + thing);
-    if (thing === '_queryType') {
-      for (const t2 in executableSchema[thing]) {
-        if (executableSchema[thing].hasOwnProperty(t2)) {
-          console.log(` .  Subname = ${t2}`);
-          const t2Obj = executableSchema[thing][t2];
-          if (t2 === 'name' || t2 === 'description') console.log(t2Obj);
-          else if (t2 === '_fields') {
-            for (const t3 in t2Obj) {
-              if (t2Obj.hasOwnProperty(t3)) {
-                console.log(` .  And the field is ${t3}`);
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
-
-/*
- * Import Firebase
- *    For now, the use of require and import of individual
- *    submodules is needed to avoid problems with webpack
- *    (import seems to require beta version of webpack 2).
- */
+// Import Firebase - for now (8/25/16), the use of require and import of individual
+// submodules is needed to avoid problems with webpack (import seems to require
+// beta version of webpack 2).
 const firebase = require('firebase');
-// firebase.initializeApp({
-//   serviceAccount: './SimpliCityII-284f9d0ebb83.json',
-//   databaseURL: 'https://simplicityii-878be.firebaseio.com',
-// });
+firebase.initializeApp({
+  serviceAccount: './SimpliCityII-284f9d0ebb83.json',
+  databaseURL: 'https://simplicityii-878be.firebaseio.com',
+});
+
+const sql = require('mssql');
+const msconfig = {
+  user: process.env.dbuser,
+  password: process.env.dbpassword,
+  server: process.env.dbhost,
+  database: process.env.database,
+  pool: {
+    max: 10,
+    min: 0,
+    idleTimeoutMillis: 30000,
+  },
+};
+
+const pool = new sql.ConnectionPool(msconfig);
+pool.on('error', err => {
+  console.log(`Got ourselves a damn error: ${err}`);
+});
+
+console.log('Here we go');
+pool.connect(err => {
+  if (err) console.log(`Error trying to create a connection pool ${err}`);
+});
+
 
 const GRAPHQL_PORT = process.env.PORT || 8080;
 console.log(`The graphql port is ${GRAPHQL_PORT}`);
@@ -53,41 +53,62 @@ const graphQLServer = express().use('*', cors());
 
 graphQLServer.use('/graphql', bodyParser.json(), apolloExpress((req, res) => {
   if (!req.headers.authorization || req.headers.authorization === 'null') {
+    console.log('NOT LOGGED IN');
     return {
       schema: executableSchema,
       context: {
+        pool,
+        employee_id: '1316',
         loggedin: false,
         token: null,
         uid: null,
         name: null,
         email: null,
+        groups: [],
+        subscriptions: null,
       },
     };
   }
   return firebase.auth().verifyIdToken(req.headers.authorization).then((decodedToken) => {
     console.log('auth-verify');
-    return {
-      schema: executableSchema,
-      context: {
-        loggedin: true,
-        token: req.headers.authorization,
-        uid: decodedToken.uid,
-        name: decodedToken.name,
-        email: decodedToken.email,
-      },
-    };
+    // Now we need to look up the employee ID
+    const query = `select EmpID from UserMap where Email = '${context.email}'`;
+    return pool.request()
+    .query(query)
+    .then(result => {
+      console.log(result);
+      if (result.recordset.length > 0) {
+        return {
+          schema: executableSchema,
+          context: {
+            pool,
+            employee_id: result.recordset[0].EmpID,
+            loggedin: true,
+            token: req.headers.authorization,
+            uid: decodedToken.uid,
+            name: decodedToken.name,
+            email: decodedToken.email,
+          },
+        };
+      }
+      return null;
+    });
   }).catch((error) => {
     if (req.headers.authorization !== 'null') {
-      console.log(`Error decoding firebase token: ${JSON.stringify(error)}`);
+      console.log(`Error decoding authentication token: ${JSON.stringify(error)}`);
     }
     return {
       schema: executableSchema,
       context: {
+        pool,
+        employee_id: '1316',
         loggedin: false,
         token: null,
         uid: null,
         name: null,
         email: null,
+        groups: [],
+        subscriptions: null,
       },
     };
   });
@@ -101,3 +122,22 @@ graphQLServer.listen(GRAPHQL_PORT, () => console.log(
   `GraphQL Server is now running on http://localhost:${GRAPHQL_PORT}/graphql`
 ));
 
+// Killing the subscription server for now.
+/*
+// WebSocket server for subscriptions
+const websocketServer = createServer((request, response) => {
+  response.writeHead(404);
+  response.end();
+});
+
+websocketServer.listen(WS_PORT, () => console.log( // eslint-disable-line no-console
+  `Websocket Server is now running on http://localhost:${WS_PORT}`
+));
+
+
+// eslint-disable-next-line
+new SubscriptionServer(
+  { subscriptionManager },
+  websocketServer
+);
+*/
